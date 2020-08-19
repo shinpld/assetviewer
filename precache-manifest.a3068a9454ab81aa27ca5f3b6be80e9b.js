312f4921403d18002d4846d3b0d401524fc4d7ec
shinpld <shinpld@gmail.com>
2020-08-19 16:49:36 +0700

    Updates

---
+++ precache-manifest.a3068a9454ab81aa27ca5f3b6be80e9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20ecf84226bf6ac7c2e7ec36a9c470d",
+    "url": "/assetviewer/index.html"
+  },
+  {
+    "revision": "3b9f6da1c7e60e98cfeb",
+    "url": "/assetviewer/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9c16409ecc98eb013319",
+    "url": "/assetviewer/static/css/main.043682d4.chunk.css"
+  },
+  {
+    "revision": "3b9f6da1c7e60e98cfeb",
+    "url": "/assetviewer/static/js/2.7d201ee7.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/assetviewer/static/js/2.7d201ee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c16409ecc98eb013319",
+    "url": "/assetviewer/static/js/main.d1a0e0f4.chunk.js"
+  },
+  {
+    "revision": "6bad4a6b550ba8b6af46",
+    "url": "/assetviewer/static/js/runtime-main.2de47d8a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/assetviewer/static/media/SEGA.12a3db8b.woff"
+  }
+]);
